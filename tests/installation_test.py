799bc8fb0757281a404e83270c990d4505d4fb82
Michael Williamson <mike@zwobble.org>
2013-01-02 23:33:39 +0000

    Use whack to install apache2 with php5

--- tests/installation_test.py
+++ tests/installation_test.py
@@ -1,7 +1,9 @@
 import os
 import subprocess
+import time
 
-from nose.tools import istest
+from nose.tools import istest, assert_equals
+from starboard import find_local_free_tcp_port as find_port
 from selenium import webdriver
 
 from tempdir import create_temporary_dir
@@ -10,23 +12,28 @@ from tempdir import create_temporary_dir
 @istest
 def deploying_wordpress_shows_front_page():
     with create_temporary_dir() as temp_dir:
-        install_wordpress(temp_dir, port=54321)
-        process = start_apache2(temp_dir)
+        port = find_port()
+        install_wordpress(temp_dir, port=port)
+        process = subprocess.Popen(
+            ["bin/httpd", "-DNO_DETACH"],
+            cwd=temp_dir
+        )
         try:
-            assert hello_world_post_is_visible(port=54321)
+            assert hello_world_post_is_visible(port=port)
         finally:
             process.terminate()
 
+
 def install_wordpress(directory, port):
     path = os.path.join(os.path.dirname(__file__), "..")
-    subprocess.check_call(["whack", "install", path, directory, "-pport={0}".format(port), "--no-cache"])
-
-def start_apache2(temp_dir):
-    apache_dir = os.path.join(temp_dir, "apache2")
-    return subprocess.Popen(
-        ["bin/httpd", "-DNO_DETACH", "-d", apache_dir],
-        cwd=apache_dir
-    )
+    subprocess.check_call(["whack", "install", path, directory, "--no-cache"])
+    
+    conf_path = os.path.join(directory, "conf/httpd.conf")
+    with open(conf_path, "r") as conf_file:
+        original_conf_contents = conf_file.read()
+    conf_contents = original_conf_contents.replace("Listen 80", "Listen {0}".format(port))
+    with open(conf_path, "w") as conf_file:
+        conf_file.write(conf_contents)
 
 def hello_world_post_is_visible(port):
     url = "http://localhost:{0}/".format(port)
